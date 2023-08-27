ca6f1c30443d51e5750ec1c36a7353074f6c359e
sasaharukimedes <sasaharu3@gmail.com>
2023-08-27 12:40:57 +0900

    change sentencesin works

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
       <Heading padding={6} minWidth="max-content">
         <Center m={2}>{`I'm Harukimedes!`}</Center>
       </Heading>
-      <Flex justifyContent="flex-end">
+      <Flex justifyContent="flex-end" m={4}>
         <Button onClick={toggleColorMode}>Change Color Mode</Button>
       </Flex>
     </>
